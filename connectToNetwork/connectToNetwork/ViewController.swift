998d980c7cd5a767cbf2301d0287d5e3139649d7
Roman_Lab <labunskij1997@gmail.com>
2018-11-28 17:59:32 +0300

    Begin

---
+++ connectToNetwork/connectToNetwork/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  connectToNetwork
+//
+//  Created by Roman Lab on 27.11.2018.
+//  Copyright © 2018 Roman_Lab. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segContr: UISegmentedControl!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var passText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    // the button action function
+    @IBAction func Tap(_ sender: Any) {
+        emailText.resignFirstResponder()
+        passText.resignFirstResponder()
+    }
+    
+    @IBAction func Action(_ sender: UIButton) {
+        if emailText.text != "" && passText.text != ""{
+            if segContr.selectedSegmentIndex == 0 {
+                Auth.auth().signIn(withEmail: emailText.text!, password: passText.text!) { (user, error) in
+                    if user != nil{
+                        print("Success")
+                    }
+                    else{
+                        if let myError = error?.localizedDescription{
+                            print(myError)
+                        }
+                        else{
+                            print("error!")
+                        }
+                    }
+                }
+            }
+            else{
+                Auth.auth().createUser(withEmail: emailText.text!, password: passText.text!) { (user, error) in
+                    if user != nil{
+                        print("Success")
+                    }
+                    else{
+                        if let myError = error?.localizedDescription{
+                            print(myError)
+                        }
+                        else{
+                            print("error!")
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
