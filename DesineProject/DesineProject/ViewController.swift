a9097e078b77cdebcf97d56860feb4a9b0b53944
QuaDref <33463054+QuaDref@users.noreply.github.com>
2018-12-01 04:53:52 +0300

    view

---
+++ DesineProject/DesineProject/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DesineProject
+//
+//  Created by Lakki on 30.11.2018.
+//  Copyright © 2018 Lakki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Прозрачный navigation item
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationController?.navigationBar.shadowImage = UIImage()
+        
+        
+        
+        
+        }
+    
+   
+    
+    }
+
+
