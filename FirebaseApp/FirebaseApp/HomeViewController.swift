f2cc37d23d759e3b7bf4c583f0e38047db3752bb
Roman_Lab <labunskij1997@gmail.com>
2018-12-01 04:06:03 +0300

    Авторизация/Регистрация

---
+++ FirebaseApp/FirebaseApp/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  MOTM Alpha
+//
+//  Created by Roman Lab on 01.12.2018.
+//  Copyright © 2018 Roman_Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ sender:Any) {
+        try! Auth.auth().signOut()
+        self.dismiss(animated: false, completion: nil)
+    }
+}
---
+++ FirebaseApp/FirebaseApp/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  FirebaseApp
+//
+//  Created by Roman Lab on 01.12.2018.
+//  Copyright © 2018 Robert Canton. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
